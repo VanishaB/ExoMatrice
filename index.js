0cdf2e814fa0c56eccf62f6852715c2dc72d3ded
VanishaB <vanishabhujun@hotmail.fr>
2018-06-17 14:52:20 +0200

    modif partie2

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
     $("div1").click(function(){
-        $(".form").addClass("important blue");
+        $(".form").addClass('blue');
     });
 });
 
 $(document).ready(function(){
     $("div2").click(function(){
-        $(".form").removeClass("blue");
+        $(".form").removeClass('blue');
     });
 });
 
 $( ".rond" ).click(function() {
-  $( this ).toggleClass( "blue" );
+  $( this ).toggleClass( 'blue' );
 });
 
-           
+
+ $(".carre").click(function(){
+      if ($(this).hasClass("row1") ) $(".row1").toggleClass('blue');
+      if ($(this).hasClass("row2") ) $(".row2").toggleClass('blue');
+      if ($(this).hasClass("row3") ) $(".row3").toggleClass('blue');
+      if ($(this).hasClass("row4") ) $(".row4").toggleClass('blue');
+      if ($(this).hasClass("row5") ) $(".row5").toggleClass('blue');
+
+    });
+
+    $(".losange").click(function(){
+      $(".row3, .col1").toggleClass('blue');
+    });
